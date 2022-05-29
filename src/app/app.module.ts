d43726acb11f30e66324657fe7fea64b0e025ffa
Serhii Bohush <tpf384@motorolasolutions.com>
2022-05-29 19:48:08 +0300

    start front project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';import { FormsModule } from '@angular/forms';
+import { EmployeeService } from './employee.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, HttpClientModule, FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
